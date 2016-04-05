93a9621c4878304f70bd4d14ff55eafacac9ff71
mahmudh456 <mahmudhs949@gmail.com>
2016-04-05 23:16:48 +0600

    Update and rename k1_turbo-vendor.mk to RX4-vendor.mk

---
+++ RX4-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Walton/RX4/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Walton/RX4/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Walton/RX4/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Walton/RX4/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Walton/RX4/proprietary/vendor,system/vendor)
